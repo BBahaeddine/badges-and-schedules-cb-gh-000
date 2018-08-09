8b4f7c5d6828f0ecc101e411959c3cf318c7e4c5
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 09:25:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+end
+
+def assign_rooms(speakers_list)
+  assignments = []
+  speakers_list.each_with_index { |speaker, index|
+  assignments << "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+  return assignments
+end
+
+def printer
+end
+
+assign_rooms(["Baha", "Me", "you"]).inspect()
